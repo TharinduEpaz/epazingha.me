cad03fddcf648c0aa9a3ff8ea356aa821edf79b0
tharindu.epasingha <tharindu.epasingha@itechro.com>
2024-12-19 09:38:18 +0530

    added the main spinner

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,56 @@
+import { Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import { RingLoader } from "react-spinners";
 import { Experience } from "./components/Experience";
-import { Html, Scroll, ScrollControls } from "@react-three/drei";
 import Interface from "./components/Interface";
 import Navbar from "./components/Navbar";
-import { useState } from "react";
 import { ScrollManager } from "./components/ScrollManager";
-
 
 function App() {
   const [section, setSection] = useState(0);
-  
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
-    <Navbar section={section} setSection={setSection}/>
-    <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      
-      
-      <ScrollControls pages={4} damping={0.3}>
-      <ScrollManager section={section} setSection={setSection} />
-      <Scroll>
-      <Experience section={section} setSection={setSection} />
-      
-      </Scroll>
-     
-      <Scroll html>
-      
-      <Interface section={section} setSection={setSection}/>
-      </Scroll>
-      </ScrollControls>
-    </Canvas>
+      {isLoading && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9999,
+          }}
+        >
+          <RingLoader color="blue" size={100} />
+          <p className="text-2xl font-bold ml-4">Loading 3D experience...</p>
+        </div>
+      )}
+      <Navbar section={section} setSection={setSection} />
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 5], fov: 30 }}
+        onCreated={() => setIsLoading(false)}
+      >
+        <color attach="background" args={["#ececec"]} />
+
+        <ScrollControls pages={4} damping={0.3}>
+          <ScrollManager section={section} setSection={setSection} />
+          <Scroll>
+            <Experience section={section} setSection={setSection} />
+          </Scroll>
+
+          <Scroll html>
+            <Interface section={section} setSection={setSection} />
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
     </>
   );
 }
